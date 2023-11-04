e46cc5d9ee25a65cfd17cdc548a433f0b85cae83
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-11-03 21:07:07 -0700

    Mudanças na HomeView, adicionando Rounterlink e mudança de icone

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 
+
+
 const showPassword = ref(false)
 </script>
 <template>
@@ -9,10 +11,10 @@ const showPassword = ref(false)
       <img src="/logo-tela-branca.png" alt="Logo fábrica de software">
     </div>
     <div class="flex flex-col h-1/2 text-start w-2/3 gap-3">
-      <h1 class="text-3xl font-bold text-center ">Login</h1>
+      <h1 class="text-3xl font-bold text-center">Login</h1>
       <div class="flex flex-col gap-5">
         <div class="relative flex items-center">
-          <i class="fa-solid fa-envelope absolute pl-4 text-2xl"></i>
+          <i class="fa-solid fa-user absolute pl-4 text-2xl"></i>
           <input type="text" placeholder="Dono-TAG" class="bg-transparent p-4 border-2 rounded-2xl border-black outline-none w-full pl-12">
         </div>
         <div class="relative flex items-center ">
@@ -29,7 +31,12 @@ const showPassword = ref(false)
           </div>
         </div>
       </div>
+
+      <div class="flex flex-col h-1/2 text-center w-3/3 gap-3">
+      <RouterLink :to=" {name: 'main'}">
       <button class="bg-cyan-700 p-4 rounded-2xl text-xl text-white font-black">Login</button>
+    </RouterLink>
+    </div>
     </div>
   </div>
   
