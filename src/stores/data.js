ef042d32a4c6647b5215a129fe3bd89cc83e4a5d
snow-sr <joaofelipi877@gmail.com>
2023-11-23 16:17:19 -0300

    Add socket.io client and update data fetching

--- src/stores/data.js
+++ src/stores/data.js
@@ -1,6 +1,7 @@
 import api from '../plugins/axios.js'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { io } from 'socket.io-client'
 
 export const useDataStore = defineStore('data', () => {
   const data = ref({
@@ -11,14 +12,31 @@ export const useDataStore = defineStore('data', () => {
     lastUsed: ''
   })
 
+  const socket = io('https://porta.app1.fabricadesoftware.ifc.edu.br/', {
+    reconnectionDelayMax: 10000
+  })
+
+  socket.on('connect', () => {
+    console.log('connected')
+  })
+
+  socket.on('updateFront', (e) => {
+    if (e) {
+      fetchData()
+    }
+  })
+
   const fetchData = async () => {
     const response = await api.get('users')
+    data.value.users = []
     data.value.users = response.data
 
     const tags = await api.get('rfid')
+    data.value.tags = []
     data.value.tags = tags.data
 
     const last = await api.get('last')
+    data.value.lastUsed = ''
     data.value.lastUsed = last.data
 
     data.value.tags.forEach((tag) => {
